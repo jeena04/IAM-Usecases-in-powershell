d5e23a0da2921baca8b54ac7e1266cd375b72697
Jeena Sebastian <jeena4mampilly@gmail.com>
2024-04-06 19:57:24 +0530

    added the script for fetching multiple group members at a time

---
+++ BulkADgpMemberS_Data.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module ActiveDirectory
+
+$groups = get-content "$home\Desktop\groups.txt"
+
+foreach ($group in $groups) {
+
+	$res = Get-ADGroupMember -Identity $group | ?{$_.ObjectClass -eq "user"} | ft -HideTableHeaders SamAccountName,name -auto | Out-String 
+	Get-ADGroupMember -Identity $group | ?{$_.ObjectClass -eq "Group"} | %{Write-Output "`n"$_.Name;Get-ADGroupMember $_ } | ft -HideTableHeaders SamAccountName,name -auto > $home\Desktop\$group.txt
+	Add-Content $home\Desktop\$group.txt $group
+	Add-Content $home\Desktop\$group.txt $res
+
+}
