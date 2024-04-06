c6a1ac8f174cb4c13bc0fee671569a00745a1739
Jeena Sebastian <jeena4mampilly@gmail.com>
2024-04-06 20:11:16 +0530

    Comments added and explained the line of code

--- AccesslistPath.ps1
+++ AccesslistPath.ps1
@@ -1,11 +1,24 @@
-﻿$path="path"
+﻿#path which we need users list
+$path="path"
+#output file path where we need to store the data
 $outputpath="path"
+#Reading the folder structure and recurse to the subfolders and hidden folders and storing to variable
 $FolderPath = Get-ChildItem -Directory -Path $path -Recurse -Force
+
+#initialising func and iterating to the folders read
 $Output = @()
 ForEach ($Folder in $FolderPath) {
+
+#getting access control list for folders and storing the permission
     $Acl = Get-Acl -Path $Folder.FullName
+
+    #looping through access control entries
     ForEach ($Access in $Acl.Access) {
+
+    #the retrieved properties of each access entries are stored in hashtable
 $Properties = [ordered]@{'Folder Name'=$Folder.FullName;'Group/User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+
+#custom obejects created and storing the defined hashtable data
 $Output += New-Object -TypeName PSObject -Property $Properties            
 }
 }
