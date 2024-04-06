8cabc6aecf511d65ae8e5ba9f2d0cb41a0cdc454
Jeena Sebastian <jeena4mampilly@gmail.com>
2024-04-06 20:01:24 +0530

    Added Script for getting users list who have access to specific path

---
+++ AccesslistPath.ps1
@@ -0,0 +1,12 @@
+﻿$path="path"
+$outputpath="path"
+$FolderPath = Get-ChildItem -Directory -Path $path -Recurse -Force
+$Output = @()
+ForEach ($Folder in $FolderPath) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    ForEach ($Access in $Acl.Access) {
+$Properties = [ordered]@{'Folder Name'=$Folder.FullName;'Group/User'=$Access.IdentityReference;'Permissions'=$Access.FileSystemRights;'Inherited'=$Access.IsInherited}
+$Output += New-Object -TypeName PSObject -Property $Properties            
+}
+}
+$Output | Export-Csv -path $outputpath
