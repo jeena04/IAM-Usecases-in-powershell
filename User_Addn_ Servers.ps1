2bcda93f813858af56d633df12e0a7899dbabed8
Jeena Sebastian <jeena4mampilly@gmail.com>
2024-04-06 19:45:41 +0530

    initial script version

---
+++ User_Addn_ Servers.ps1
@@ -0,0 +1,16 @@
+﻿$inputpath="path"
+$outputpath="Path"
+$servers = Get-Content $inputpath
+"Name`tStatus" | Out-File -FilePath C:\Users\w3919\Desktop\results.txt
+foreach ($server in $servers){
+ try{
+  $adminGroup = [ADSI]"WinNT://$server/Administrators"
+  $adminGroup.add("WinNT://domain/ID")
+  "$server`tSuccess"
+  "$server`tSuccess" | Out-File -FilePath C:\Users\w3919\Desktop\results.txt -Append
+ }
+ catch{
+  "$server`t" + $_.Exception.Message.ToString().Split(":")[1].Replace("`n","")
+  "$server`t" + $_.Exception.Message.ToString().Split(":")[1].Replace("`n","") | Out-File -FilePath $outputpath -Append
+ }
+}
