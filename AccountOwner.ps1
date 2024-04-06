ac8ca56afba0a818000a60fb7e92a12e459698d2
Jeena Sebastian <jeena4mampilly@gmail.com>
2024-04-06 20:16:19 +0530

    Created code for getting properties of accounts

--- AccountOwner.ps1
+++ AccountOwner.ps1
@@ -1,1 +1,13 @@
-﻿
+﻿Import-Module ActiveDirectory
+$inputpath="path"
+$outfile="output filepath"
+$users = ForEach ($user in $( Get-Content "$inputpath" ))
+{
+
+Get-AdUser $user -Properties managedBy, Notes,rSAAccountType
+
+}
+
+$users |
+Select-Object SamAccountName, managedBy, Notes,rSAAccountType |
+Export-CSV -Path  $outfile -NoTypeInformation
